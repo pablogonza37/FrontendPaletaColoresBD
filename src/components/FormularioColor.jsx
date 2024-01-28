6dd5aaa0b30d3be3cf9145a29e1eef6e8fa70785
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-28 00:04:26 -0300

    se creo´la funcion para guardar en localstorage

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -2,23 +2,53 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ListaColores from "./ListaColores";
 import { Col, Row, Form } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const FormularioColor = () => {
   const [color, setColor] = useState("");
-  const [colores, setColores] = useState([]);
+  const coloresLocalStorage = JSON.parse(localStorage.getItem('coloresKey')) || [] 
+  const [colores, setColores] = useState(coloresLocalStorage);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setColores([...colores, color]);
+
+    if (!error && color) {
+      setColores([...colores, color]);
     setColor('');
+    } else {
+      alert('Color inválido');
+    }
+    
   };
 
-  const borrarColor = (nombreColor)=>{
-    const ColoresFiltrados = colores.filter((color)=>color !== nombreColor); 
-    setColores(ColoresFiltrados);
-  }
+  const borrarColor = (nombreColor) => {
+    const indiceColor = colores.findIndex(color => color === nombreColor);
+    if (indiceColor !== -1) {
+        const nuevosColores = [...colores.slice(0, indiceColor), ...colores.slice(indiceColor + 1)];
+        setColores(nuevosColores);
+    }
+}
+
+  useEffect(()=>{
+    localStorage.setItem('coloresKey', JSON.stringify(colores))
+  },[colores])
+
+ 
+    const [error, setError] = useState('');
+  
+    const handleInputChange = (e) => {
+      const inputValue = e.target.value.toLowerCase(); 
+      const colorRegex = /^(red|green|blue|yellow|orange|purple|pink|cyan|magenta)$/i;
+  
+      setColor(inputValue);
+  
+      if (!colorRegex.test(inputValue)) {
+        setError('Ingrese un nombre de color válido');
+      } else {
+        setError('');
+      }
+    };
 
   return (
     <section>
@@ -39,10 +69,13 @@ const FormularioColor = () => {
                 >
                   <Form.Control
                     type="text"
-                    placeholder="Ingrese un color ej: blue"
+                    placeholder="Ingrese un color ej: red"
                     minLength={3}
                     maxLength={10}
-                    onChange={(e) => setColor(e.target.value)}
+                    onChange={(e) => {
+                      setColor(e.target.value);
+                      handleInputChange(e);
+                    }}
                     value={color}
                   />
                 </Form.Group>
